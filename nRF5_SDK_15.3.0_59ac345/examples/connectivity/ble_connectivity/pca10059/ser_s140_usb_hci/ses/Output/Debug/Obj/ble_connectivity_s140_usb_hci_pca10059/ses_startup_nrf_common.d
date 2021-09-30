52dd9b150ce51adff78bb6e5128b715b5b53243f
OS100 <luegera@hotmail.com>
2021-09-30 23:04:02 +0200

    clean

--- nRF5_SDK_15.3.0_59ac345/examples/connectivity/ble_connectivity/pca10059/ser_s140_usb_hci/ses/Output/Debug/Obj/ble_connectivity_s140_usb_hci_pca10059/ses_startup_nrf_common.d
+++
@@ -1,2 +0,0 @@
-Output/Debug/Obj/ble_connectivity_s140_usb_hci_pca10059/ses_startup_nrf_common.o: \
- C:\Users\Besitzer\Documents\GitHub\ruuvi.firmware.c\nRF5_SDK_15.3.0_59ac345\modules\nrfx\mdk\ses_startup_nrf_common.s
--- nRF5_SDK_15.3.0_59ac345/examples/connectivity/ble_connectivity/pca10059/ser_s140_usb_hci/ses/Output/Debug/Obj/ble_connectivity_s140_usb_hci_pca10059/thumb_crt0.d
+++
@@ -1,2 +0,0 @@
-Output/Debug/Obj/ble_connectivity_s140_usb_hci_pca10059/thumb_crt0.o: \
- C:/Program\ Files/SEGGER/SEGGER\ Embedded\ Studio\ for\ ARM\ 5.62/source/thumb_crt0.s
